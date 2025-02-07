d65b4979ef0284f3dd16679abcc473e6c00441f2
yusa0730 <isizawa5226@gmail.com>
2025-02-07 13:18:29 +0900

    add: ses

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -5,10 +5,6 @@
 
 declare module "sst" {
   export interface Resource {
-    "TestBucketAfterMove3": {
-      "name": string
-      "type": "sst.aws.Bucket"
-    }
   }
 }
 /// <reference path="sst-env.d.ts" />
--- sst.config.ts
+++ sst.config.ts
@@ -1,94 +1,95 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 import type { DistributionArgs } from "@pulumi/aws/cloudfront";
+import * as pulumi from "@pulumi/pulumi";
 
 export default $config({
   app(input) {
     return {
-      name: "aws-remix-after-move3",
+      name: "aws-remix-after-move4",
       home: "aws",
     };
   },
   async run() {
-    const idPrefix = "ishizawa-test"
+    const idPrefix = "ishizawa-test4"
     const domainName = "ishizawa-test.xyz";
-    const bucket = new sst.aws.Bucket("TestBucketAfterMove3", {
-      access: "public"
-    });
-
     const hostedZone = await aws.route53.getZone({
       name: `${domainName}.`,
     });
 
-    console.log("=======hostedZone=======", hostedZone.zoneId);
+    // const bucket = new sst.aws.Bucket("TestBucketAfterMove4", {
+    //   access: "public"
+    // });
 
-    const useast1 = new aws.Provider("Useast1TestAfterMove3", {
-      region: "us-east-1",
-    });
+    // console.log("=======hostedZone=======", hostedZone.zoneId);
 
-    // 移行元のCloudFrontで設定しているACMを取得する
-    const acm = await aws.acm.getCertificate(
-      {
-        domain: domainName,
-        statuses: ["ISSUED"],
-      },
-      { provider: useast1 },
-    );
+    // const useast1 = new aws.Provider("Useast1TestAfterMove4", {
+    //   region: "us-east-1",
+    // });
 
-    console.log("====acm====", acm.arn)
+    // // 移行元のCloudFrontで設定しているACMを取得する
+    // const acm = await aws.acm.getCertificate(
+    //   {
+    //     domain: domainName,
+    //     statuses: ["ISSUED"],
+    //   },
+    //   { provider: useast1 },
+    // );
 
-    const remix = new sst.aws.Remix("RemixTestAfterMove3", {
-      link: [bucket],
-      // domain: "ishizawa-test.xyz"
-      transform: {
-        cdn: (
-          args: sst.aws.CdnArgs,
-          opts: $util.CustomResourceOptions,
-          name: string
-        ) => {
-          console.log("====acm====", acm.arn)
-          args.transform = {
-            distribution: (
-              args: DistributionArgs,
-              opts: $util.CustomResourceOptions,
-              name: string
-            ) => {
-              args.viewerCertificate = {
-                acmCertificateArn: acm.arn,
-                minimumProtocolVersion: "TLSv1.2_2021",
-                sslSupportMethod: "sni-only",
-              };
+    // console.log("====acm====", acm.arn)
 
-              args.aliases = [domainName]
-            },
-          };
-        }
-      }
-    });
+    // const remix = new sst.aws.Remix("RemixTestAfterMove3", {
+    //   link: [bucket],
+    //   // domain: "ishizawa-test.xyz"
+    //   transform: {
+    //     cdn: (
+    //       args: sst.aws.CdnArgs,
+    //       opts: $util.CustomResourceOptions,
+    //       name: string
+    //     ) => {
+    //       console.log("====acm====", acm.arn)
+    //       args.transform = {
+    //         distribution: (
+    //           args: DistributionArgs,
+    //           opts: $util.CustomResourceOptions,
+    //           name: string
+    //         ) => {
+    //           args.viewerCertificate = {
+    //             acmCertificateArn: acm.arn,
+    //             minimumProtocolVersion: "TLSv1.2_2021",
+    //             sslSupportMethod: "sni-only",
+    //           };
 
-    remix.nodes.cdn?.apply((cdn) => {
-      console.log(cdn);
-      cdn.url.apply((url) => {
-        console.log("====url===", url);
+    //           args.aliases = [domainName]
+    //         },
+    //       };
+    //     }
+    //   }
+    // });
 
-        // `https://` を削除した新しい変数
-        const cleanUrl = url.replace(/^https?:\/\//, "");
-        console.log("====cleanUrl===", cleanUrl);
+    // remix.nodes.cdn?.apply((cdn) => {
+    //   console.log(cdn);
+    //   cdn.url.apply((url) => {
+    //     console.log("====url===", url);
 
-        // Route 53 TXT record transfer domain
-        new aws.route53.Record("transfer-txt-record", {
-          zoneId: hostedZone.zoneId,
-          name: `_.${domainName}`,
-          type: aws.route53.RecordType.TXT,
-          ttl: 1800,
-          records: [$interpolate`${cleanUrl}`],
-        });
-      });
-    });
+    //     // `https://` を削除した新しい変数
+    //     const cleanUrl = url.replace(/^https?:\/\//, "");
+    //     console.log("====cleanUrl===", cleanUrl);
+
+    //     // Route 53 TXT record transfer domain
+    //     new aws.route53.Record("transfer-txt-record", {
+    //       zoneId: hostedZone.zoneId,
+    //       name: `_.${domainName}`,
+    //       type: aws.route53.RecordType.TXT,
+    //       ttl: 1800,
+    //       records: [$interpolate`${cleanUrl}`],
+    //     });
+    //   });
+    // });
 
 
-    if (!domainName) {
-      throw new Error(`Invalid domainName: ${domainName}`);
-    }
+    // if (!domainName) {
+    //   throw new Error(`Invalid domainName: ${domainName}`);
+    // }
 
   const getDomainIdentity = await aws.ses
     .getDomainIdentity({
@@ -108,6 +109,10 @@ export default $config({
         },
       );
 
+      console.log("====domainIdentity====", domainIdentity.domain.apply((domain) => {
+        return domain;
+      }))
+
       // Route 53 MX record
       new aws.route53.Record(`${idPrefix}-mx-record-${$app.stage}`, {
         zoneId: hostedZone.zoneId,
@@ -117,13 +122,13 @@ export default $config({
         records: ["10 feedback-smtp.ap-northeast-1.amazonses.com"],
       });
 
-      new aws.route53.Record(`${idPrefix}-verify-txt-record-${$app.stage}`, {
-        zoneId: hostedZone.zoneId,
-        name: `_amazonses.${domainName}`,
-        type: aws.route53.RecordType.TXT,
-        ttl: 1800,
-        records: [domainIdentity.verificationToken],
-      });
+      // new aws.route53.Record(`${idPrefix}-verify-txt-record-${$app.stage}`, {
+      //   zoneId: hostedZone.zoneId,
+      //   name: `_amazonses.${domainName}`,
+      //   type: aws.route53.RecordType.TXT,
+      //   ttl: 1800,
+      //   records: [domainIdentity.verificationToken],
+      // });
 
       // Route 53 TXT record SPF
       new aws.route53.Record(`${idPrefix}-bounce-txt-record-${$app.stage}`, {
@@ -148,30 +153,47 @@ export default $config({
       const domainDkim = new aws.ses.DomainDkim(
         `${idPrefix}-domain-dkim-${$app.stage}`,
         {
-          domain: domainIdentity.domain,
+          domain: domainName,
         },
       );
 
       const dkimRecord: aws.route53.Record[] = [];
-      for (const range = {value: 0}; range.value < 3; range.value++) {
-        console.log("こんにちは")
-        dkimRecord.push(new aws.route53.Record(`${idPrefix}-dkim-record-${range.value}-${$app.stage}`, {
-            zoneId: hostedZone.zoneId,
-            name: domainDkim.dkimTokens.apply(dkimTokens => `${dkimTokens[range.value]}._domainkey`),
-            type: aws.route53.RecordType.CNAME,
-            ttl: 600,
-            records: [domainDkim.dkimTokens.apply(dkimTokens => `${dkimTokens[range.value]}.dkim.amazonses.com`)],
-        }));
-      }
+      domainDkim.dkimTokens.apply((dkimTokens) => {
+        console.log("=======dkimTokens========", dkimTokens);
 
-      new aws.ses.MailFrom(`${idPrefix}-mail-from-${$app.stage}`, {
-        domain: domainName,
-        mailFromDomain: `bounce.${domainName}`,
+        // dkimTokens の各値を処理
+        dkimTokens.forEach((dkimToken, index) => {
+          console.log("=======dkimToken========", dkimToken);
+
+          dkimRecord.push(new aws.route53.Record(`${idPrefix}-dkim-record-${index}-${$app.stage}`, {
+            zoneId: hostedZone.zoneId,
+            name: `${dkimToken}._domainkey`,
+            type: aws.route53.RecordType.CNAME,
+            ttl: 1800,
+            records: [`${dkimToken}.dkim.amazonses.com`],
+          }));
+        });
       });
+
+
+      $util
+        .all([domainIdentity.verificationToken])
+        .apply(([verificationToken]) => {
+          console.log(`Verification Token: ${verificationToken}`);
+          
+          return new Promise((resolve) => {
+            resolve(
+              new aws.ses.MailFrom(`${idPrefix}-mail-from-${$app.stage}`, {
+                domain: domainName,
+                mailFromDomain: `bounce.${domainName}`,
+              })
+            );
+          });
+        });
     });
 
     if (getDomainIdentity) {
-      const domainIdentity = new aws.ses.DomainIdentity(
+      new aws.ses.DomainIdentity(
         `${idPrefix}-domain-identity-${$app.stage}`,
         {
           domain: domainName,
@@ -196,13 +218,13 @@ export default $config({
         },
       );
 
-      new aws.route53.Record(`${idPrefix}-verify-txt-record-${$app.stage}`, {
-        zoneId: hostedZone.zoneId,
-        name: `_amazonses.${domainName}`,
-        type: aws.route53.RecordType.TXT,
-        ttl: 1800,
-        records: [domainIdentity.verificationToken],
-      });
+      // new aws.route53.Record(`${idPrefix}-verify-txt-record-${$app.stage}`, {
+      //   zoneId: hostedZone.zoneId,
+      //   name: `_amazonses.${domainName}`,
+      //   type: aws.route53.RecordType.TXT,
+      //   ttl: 1800,
+      //   records: [domainIdentity.verificationToken],
+      // });
 
       // Route 53 TXT record SPF
       new aws.route53.Record(
@@ -239,7 +261,7 @@ export default $config({
       const domainDkim = new aws.ses.DomainDkim(
         `${idPrefix}-domain-dkim-${$app.stage}`,
         {
-          domain: domainIdentity.domain,
+          domain: domainName,
         },
         {
           import: domainName,
@@ -248,28 +270,27 @@ export default $config({
 
       const dkimRecord: aws.route53.Record[] = [];
       domainDkim.dkimTokens.apply((dkimTokens) => {
-        console.log("===dkimTokens====", dkimTokens)
-        if (!dkimTokens || dkimTokens.length < 3) {
-          throw new Error("DKIM Tokens are not available or incomplete");
-        }
+        console.log("=======dkimTokens========", dkimTokens);
 
-        for (let i = 0; i < 3; i++) {
+        // dkimTokens の各値を処理
+        dkimTokens.forEach((dkimToken, index) => {
+          console.log("=======dkimToken========", dkimToken);
+
           dkimRecord.push(
             new aws.route53.Record(
-              `${idPrefix}-dkim-record-${i}-${$app.stage}`,
+              `${idPrefix}-dkim-record-${index}-${$app.stage}`,
               {
                 zoneId: hostedZone.zoneId,
-                name: `${dkimTokens[i]}._domainkey`,
+                name: `${dkimToken}._domainkey`,
                 type: aws.route53.RecordType.CNAME,
                 ttl: 1800,
-                records: [`${dkimTokens[i]}.dkim.amazonses.com`],
+                records: [`${dkimToken}.dkim.amazonses.com`],
               },
               {
-                import: `${hostedZone.zoneId}_${dkimTokens[i]}._domainkey_CNAME`,
+                import: `${hostedZone.zoneId}_${dkimToken}._domainkey_CNAME`,
               },
-            ),
-          );
-        }
+          ));
+        });
       });
 
       new aws.ses.MailFrom(
