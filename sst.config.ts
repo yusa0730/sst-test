b82f372f622a986d951f440943b9fba538f2bac6
yusa0730 <isizawa5226@gmail.com>
2025-03-26 17:03:04 +0900

    add: cloudfront

--- sst.config.ts
+++ sst.config.ts
@@ -9,17 +9,21 @@ export default $config({
   },
   async run() {
     await import('./infra/infra-config');
-    await import('./infra/iam');
-    await import('./infra/cloudwatch');
-    await import('./infra/vpc');
-    await import('./infra/security-group');
-    await import('./infra/acm');
-    await import('./infra/s3');
-    await import('./infra/alb');
-    await import('./infra/ecr');
+    // await import('./infra/iam');
+    // await import('./infra/cloudwatch');
+    // await import('./infra/vpc');
+    // await import('./infra/security-group');
+    // await import('./infra/acm');
+    // await import('./infra/s3');
+    // await import('./infra/alb');
+    // await import('./infra/ecr');
     // await import('./infra/ecs');
-    await import('./infra/ecs-sst');
+    // await import('./infra/ecs-sst');
     // await import('./infra/remix');
     // await import('./infra/ses');
+
+    // =======署名付きURLのテスト======
+    await import('./infra/s3');
+    await import('./infra/cloudfront');
   }
 });
